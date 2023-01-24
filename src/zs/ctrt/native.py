8df27c12c891a07ad654316a8b4ef3ce9d1d00bf
Binyamin Y Cohen <binyamincohen555@gmail.com>
2023-01-24 14:03:29 +0200

    Add better interop support

--- src/zs/ctrt/native.py
+++ src/zs/ctrt/native.py
@@ -2,55 +2,77 @@
 This file contains utilities to create facade classes between the Python backend and the
 Z# interpreter.
 """
+# from inspect import get_annotations
+from functools import partial
+from typing import TypeVar, Generic, Callable, Any, Union
 
-from typing import TypeVar, Generic
-
-from .core import _Object, _ObjectType, _AnyType, _TypeType
+from .core import _Object, _ObjectType, _AnyType
 from .errors import UnknownFieldError
 from .protocols import *
+from .protocols import CallableProtocol, ObjectProtocol
 from ..ast.node_lib import While
 
 
 class _TypeMeta(type, _ObjectType):
     _fields: dict[str, "NativeField"]
+    _items: dict[str, Union["NativeField", "NativeFunction"]]
+    _methods: dict[str, "NativeFunction"]
 
     def __init__(cls, name: str, bases: tuple, namespace: dict):
         super().__init__(name, bases, namespace)
         cls._fields = dict(namespace.get("_fields", ()))
+        cls._methods = dict(namespace.get("_methods", ()))
+        cls._items = dict(namespace.get("_items", ()))
+        # _ObjectType.__init__(cls)
+        # for name, item in namespace.items():
+        #     if isinstance(item, NativeFunction):
+        #         cls.add_method(name, item)
+        #     elif isinstance(item, NativeField):
+        #         cls.add_field(name, item.type, item.value)
         cls._fields.update({
-            name: NativeField(name, item) for name, item in namespace.items() if isinstance(item, ObjectProtocol)
+            getattr(item, "name", name): item for name, item in namespace.items() if isinstance(item, ObjectProtocol) and isinstance(item, NativeField)
         })
+        cls._methods.update({
+            getattr(item, "name", name): item for name, item in namespace.items() if isinstance(item, ObjectProtocol) and isinstance(item, NativeFunction)
+        })
+        cls._items.update(cls._fields)
+        cls._items.update(cls._methods)
 
     @property
     def fields(self):
         return list(self._fields.values())
 
-    def create_instance(self) -> ObjectProtocol:
-        raise RuntimeError
-
     def get_name(self, instance: ObjectProtocol | None, name: str) -> ObjectProtocol | None:
         if not self.is_instance(instance):
             raise TypeError
-        if name not in self._fields:
-            raise UnknownFieldError()
         try:
-            return getattr(instance, name)
-        except AttributeError:
+            attr = self._items[name]
+            if isinstance(attr, BindProtocol):
+                attr = attr.bind([instance])
+        except KeyError:
             if self is _TypeMeta:
                 raise UnknownFieldError()
             return self.get_type().get_name(self, name)
+        else:
+            if isinstance(attr, GetterProtocol):
+                attr = attr.get()
+            return attr
 
     def set_name(self, instance: ObjectProtocol | None, name: str, value: ObjectProtocol):
         if not self.is_instance(instance):
             raise TypeError
-        if name not in self._fields:
-            raise UnknownFieldError()
         try:
-            return setattr(instance, name, value)
-        except AttributeError:
+            attr = self._items[name]
+            if isinstance(attr, BindProtocol):
+                attr = attr.bind([instance])
+        except KeyError:
             if self is _TypeMeta:
                 raise UnknownFieldError()
-            return self.get_type().set_name(self, name)
+            return self.get_type().set_name(self, name, value)
+        if isinstance(attr, SetterProtocol):
+            attr.set(value)
+        else:
+            setattr(instance, name, value)
 
     def assignable_to(self, target: "TypeProtocol") -> bool:
         if isinstance(target, _TypeMeta):
@@ -100,11 +122,13 @@ class _TypeMeta(type, _ObjectType):
 _T = TypeVar("_T")
 
 
-class NativeField:
+class NativeField(GetterProtocol, SetterProtocol):
     class DefaultType(_AnyType):
         @classmethod
         def default(cls) -> ObjectProtocol:
             return None
+
+    DefaultType = DefaultType()
 
     def __init__(self, name, value):
         self.name = name
@@ -114,14 +138,22 @@ class NativeField:
             self.type = self.DefaultType
         self.value = value
 
+    def get(self):
+        return self.value
+
+    def set(self, value: ObjectProtocol):
+        if not value.get_type().assignable_to(self.type):
+            raise TypeError
+        self.value = value
+
 
 class NativeObject(_Object, metaclass=_TypeMeta):
     def __init__(self):
         super().__init__(type(self))
 
     def __setattr__(self, name, value):
-        if name not in self._fields:
-            self._fields[name] = NativeField(name, value)
+        if name not in self._items:
+            self._items[name] = value
         super().__setattr__(name, value)
 
 
@@ -184,11 +216,32 @@ class _Type(metaclass=_TypeMeta):
     ...
 
 
+# interop utility
+
+
+def native_fn(name: str = None):
+    def wrapper(fn):
+        if not callable(fn):
+            raise TypeError
+
+        return NativeFunction(fn, name)
+
+    return wrapper
+
+
+# native types
+
+
 class String(NativeValue[str]):
     class Type(_Type):
         @classmethod
         def default(cls):
             return String("")
+
+        @native_fn("_+_")
+        @staticmethod
+        def add(left: "String", right: "String"):
+            return String(left.native + right.native)
 
 
 class Int64(NativeValue[int]):
